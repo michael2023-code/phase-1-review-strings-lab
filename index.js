484e3e5f91dad606307deb7650b060d79245e856
michael2023-code <michael.mutunga@student.moringaschool.com>
2023-07-11 09:04:21 +0300

    It is complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser="Mike"
+const welcomeMessage= "Welcome to Flatbook, " +currentUser +"!"
+const excitedWelcomeMessage= "WELCOME TO FLATBOOK, MIKE, "+"!"
+const shortGreeting= "Welcome, M, "+"!"
